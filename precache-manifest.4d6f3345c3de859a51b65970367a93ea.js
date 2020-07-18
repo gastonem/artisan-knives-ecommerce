8ea8e92fe616e308a330605bee1ef7868657d8ad
Liz Gaston <liz.gaston828@gmail.com>
2020-07-17 20:44:52 -0500

    Updates

---
+++ precache-manifest.4d6f3345c3de859a51b65970367a93ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9214115ea22b0e96b0f1ec3d1179a39",
+    "url": "/artisan-knives-ecommerce/index.html"
+  },
+  {
+    "revision": "edaca6e0ac7248c74ae0",
+    "url": "/artisan-knives-ecommerce/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f4d495773862f2445060",
+    "url": "/artisan-knives-ecommerce/static/css/main.721cfc32.chunk.css"
+  },
+  {
+    "revision": "edaca6e0ac7248c74ae0",
+    "url": "/artisan-knives-ecommerce/static/js/2.04827f1e.chunk.js"
+  },
+  {
+    "revision": "f4d495773862f2445060",
+    "url": "/artisan-knives-ecommerce/static/js/main.1d12f76f.chunk.js"
+  },
+  {
+    "revision": "f2d2a9bd8b3c1bf76ebb",
+    "url": "/artisan-knives-ecommerce/static/js/runtime~main.460dc90f.js"
+  },
+  {
+    "revision": "103184173262c256057779115e426da2",
+    "url": "/artisan-knives-ecommerce/static/media/knife.10318417.svg"
+  }
+]);
